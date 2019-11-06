b23b64101edb9b0684ac9a1a414e62f4f68ca157
_dn <dennerepin@users.noreply.github.com>
2019-11-06 17:30:01 +0100

    - update Bees model  - fix typos in LST model

--- stochnet_v2/CRN_models/Bees.py
+++ stochnet_v2/CRN_models/Bees.py
@@ -6,104 +6,80 @@ from stochnet_v2.CRN_models.base import BaseCRNModel
 
 class Bees(BaseCRNModel):
 
-    def __init__(self, endtime, timestep):
+    def __init__(
+            self,
+            endtime,
+            timestep,
+            n_bees=100,
+            non_aggressive_frac=0.2,
+    ):
 
         super().__init__(
             endtime=endtime,
             timestep=timestep,
-            model_name="Gene",
+            model_name="Bees",
         )
 
-        N = 100
-        F_init_val = 0
+        P_init_val = 100
+        n_aggr = int(n_bees * (1 - non_aggressive_frac))
+        n_non_aggr = n_bees - n_aggr
 
-        # stinging_frac = 0.85
-        # A_init_val = int(N * stinging_frac)
-        A_init_val = 50
-        NA_init_val = 50
-
-        Bee = gillespy.Species(name='Bee', initial_value=N)
-        BeeA = gillespy.Species(name='BeeA', initial_value=0)
-        BeeNA = gillespy.Species(name='BeeNA', initial_value=0)
+        Bee = gillespy.Species(name='Bee', initial_value=n_non_aggr)
+        BeeA = gillespy.Species(name='BeeA', initial_value=n_aggr)
         BeeD = gillespy.Species(name='BeeD', initial_value=0)
-        A = gillespy.Species(name='A', initial_value=A_init_val)
-        NA = gillespy.Species(name='NA', initial_value=NA_init_val)
-        F = gillespy.Species(name='F', initial_value=F_init_val)
+        P = gillespy.Species(name='P', initial_value=P_init_val)
 
         self.add_species([
             Bee,
             BeeA,
-            BeeNA,
             BeeD,
-            A,
-            NA,
-            F,
+            P,
         ])
 
-        become_aggressive_rate = gillespy.Parameter(name='become_aggressive_rate', expression=0.0005)
-        become_non_aggressive_rate = gillespy.Parameter(name='become_non_aggressive_rate', expression=0.0002)
-        calm_down_rate = gillespy.Parameter(name='calm_down_rate', expression=0.01)
-        stinging_rate = gillespy.Parameter(name='stinging_rate', expression=0.0001)
-        f_stinging_rate = gillespy.Parameter(name='f_stinging_rate', expression=0.005)
-        f_degradation_rate = gillespy.Parameter(name='f_degradation_rate', expression=0.01)
+        stinging_rate = gillespy.Parameter(name='stinging_rate', expression=0.01)  # 0.01,  0.03
+        p_stinging_rate = gillespy.Parameter(name='p_stinging_rate', expression=0.04)  # 0.03
+        become_aggressive_rate = gillespy.Parameter(name='become_aggressive_rate', expression=0.015)  # 0.01
+        p_degradation_rate = gillespy.Parameter(name='p_degradation_rate', expression=0.15)  # 0.15
 
         self.add_parameter([
-            become_aggressive_rate,
-            become_non_aggressive_rate,
-            calm_down_rate,
             stinging_rate,
-            f_stinging_rate,
-            f_degradation_rate,
+            p_stinging_rate,
+            become_aggressive_rate,
+            p_degradation_rate,
         ])
-
-        become_aggressive = gillespy.Reaction(
-            name='become_aggressive',
-            reactants={Bee: 1, A: 1},
-            products={BeeA: 1},
-            rate=become_aggressive_rate,
-        )
-
-        become_non_aggressive = gillespy.Reaction(
-            name='become_non_aggressive',
-            reactants={Bee: 1, NA: 1},
-            products={BeeNA: 1},
-            rate=become_non_aggressive_rate,
-        )
-
-        calm_down = gillespy.Reaction(
-            name='calm_down',
-            reactants={BeeA: 1},
-            products={Bee: 1, A: 1},
-            rate=calm_down_rate,
-        )
 
         stinging = gillespy.Reaction(
             name='stinging',
             reactants={BeeA: 1},
-            products={BeeD: 1, F: 1},
+            products={BeeD: 1, P: 1},
             rate=stinging_rate,
         )
 
-        f_stinging = gillespy.Reaction(
-            name='f_stinging',
-            reactants={BeeA: 1, F: 1},
-            products={BeeD: 1, F: 2},
-            rate=f_stinging_rate,
+        p_stinging = gillespy.Reaction(
+            name='p_stinging',
+            reactants={BeeA: 1, P: 1},
+            products={BeeD: 1, P: 2},
+            rate=p_stinging_rate,
+        )
+
+        become_aggressive = gillespy.Reaction(
+            name='become_aggressive',
+            reactants={Bee: 1, P: 1},
+            products={BeeA: 1, P: 1},
+            rate=become_aggressive_rate,
         )
 
         f_degradation = gillespy.Reaction(
             name='f_degradation',
-            reactants={F: 1},
+            reactants={P: 1},
             products={},
-            rate=f_degradation_rate,
+            rate=p_degradation_rate,
         )
 
         self.add_reaction([
-            become_aggressive,
-            become_non_aggressive,
-            calm_down,
             stinging,
-            f_stinging,
+            p_stinging,
+            become_aggressive,
             f_degradation,
         ])
 
@@ -112,16 +88,13 @@ class Bees(BaseCRNModel):
         return [
             'Bee',
             'BeeA',
-            'BeeNA',
             'BeeD',
-            'A',
-            'NA',
-            'F',
+            'P',
         ]
 
     @staticmethod
     def get_initial_state():
-        return [100, 0, 0, 0, 50, 50, 10]
+        return [80, 20, 0, 10]
 
     @classmethod
     def get_n_species(cls):
@@ -134,154 +107,7 @@ class Bees(BaseCRNModel):
         settings = np.zeros((n_settings, n_species))
 
         for i in range(n_species):
-            if i in [1, 2]:
-                settings[:, i] = 0.
-                continue
-            val = initial_state[i]
-            if val == 0:
-                low = 0
-                high = 1
-            else:
-                low = int(val * 0.1)
-                high = val + int(val * sigm)
-            settings[:, i] = np.random.randint(low, high, n_settings)
-        return settings
-
-    @classmethod
-    def get_histogram_bounds(cls, species_names_list=None):
-        n_species_for_histogram = len(cls.get_species_for_histogram())
-        histogram_bounds = [[0.5, 18.5]] * n_species_for_histogram
-        return histogram_bounds
-
-    @staticmethod
-    def get_species_for_histogram():
-        return ['Bee', 'BeeA', 'BeeNA', 'BeeD']
-
-
-class Bees_(BaseCRNModel):
-
-    def __init__(self, endtime, timestep):
-
-        super().__init__(
-            endtime=endtime,
-            timestep=timestep,
-            model_name="Gene",
-        )
-
-        N = 100
-        F_init_val = 0
-
-        # stinging_frac = 0.85
-        # A_init_val = int(N * stinging_frac)
-        A_init_val = 50
-
-        Bee = gillespy.Species(name='Bee', initial_value=N)
-        BeeA = gillespy.Species(name='BeeA', initial_value=0)
-        BeeD = gillespy.Species(name='BeeD', initial_value=0)
-        A = gillespy.Species(name='A', initial_value=A_init_val)
-        F = gillespy.Species(name='F', initial_value=F_init_val)
-
-        self.add_species([
-            Bee,
-            BeeA,
-            BeeD,
-            A,
-            F,
-        ])
-
-        become_aggressive_rate = gillespy.Parameter(name='become_aggressive_rate', expression=0.001)
-        # become_aggressive_f_rate = gillespy.Parameter(name='become_aggressive_f_rate', expression=0.01)
-        stinging_rate = gillespy.Parameter(name='stinging_rate', expression=0.0001)
-        f_stinging_rate = gillespy.Parameter(name='f_stinging_rate', expression=0.005)
-        calm_down_rate = gillespy.Parameter(name='calm_down_rate', expression=0.01)
-        f_degradation_rate = gillespy.Parameter(name='f_degradation_rate', expression=0.01)
-
-        self.add_parameter([
-            become_aggressive_rate,
-            # become_aggressive_f_rate,
-            calm_down_rate,
-            stinging_rate,
-            f_stinging_rate,
-            f_degradation_rate,
-        ])
-
-        become_aggressive = gillespy.Reaction(
-            name='become_aggressive',
-            reactants={Bee: 1, A: 1},
-            products={BeeA: 1},
-            rate=become_aggressive_rate,
-        )
-
-        # become_aggressive_f = gillespy.Reaction(
-        #     name='become_aggressive_f',
-        #     reactants={Bee: 1, A: 1, F: 1},
-        #     products={BeeA: 1, F: 1},
-        #     propensity_function='become_aggressive_f_rate * (Bee * A * F) / (Bee + BeeA + BeeD + A + F)',
-        # )
-
-        calm_down = gillespy.Reaction(
-            name='calm_down',
-            reactants={BeeA: 1},
-            products={Bee: 1, A: 1},
-            rate=calm_down_rate,
-        )
-
-        stinging = gillespy.Reaction(
-            name='stinging',
-            reactants={BeeA: 1},
-            products={BeeD: 1, F: 1},
-            rate=stinging_rate,
-        )
-
-        f_stinging = gillespy.Reaction(
-            name='f_stinging',
-            reactants={BeeA: 1, F: 1},
-            products={BeeD: 1, F: 2},
-            rate=f_stinging_rate,
-        )
-
-        f_degradation = gillespy.Reaction(
-            name='f_degradation',
-            reactants={F: 1},
-            products={},
-            rate=f_degradation_rate,
-        )
-
-        self.add_reaction([
-            become_aggressive,
-            # become_aggressive_f,
-            calm_down,
-            stinging,
-            f_stinging,
-            f_degradation,
-        ])
-
-    @staticmethod
-    def get_species_names():
-        return [
-            'Bee',
-            'BeeA',
-            'BeeD',
-            'A',
-            'F',
-        ]
-
-    @staticmethod
-    def get_initial_state():
-        return [100, 0, 0, 50, 10]
-
-    @classmethod
-    def get_n_species(cls):
-        return len(cls.get_species_names())
-
-    @classmethod
-    def get_initial_settings(cls, n_settings, sigm=0.5):
-        n_species = cls.get_n_species()
-        initial_state = cls.get_initial_state()
-        settings = np.zeros((n_settings, n_species))
-
-        for i in range(n_species):
-            if i in [1, 2]:
+            if i in [3]:
                 settings[:, i] = 0.
                 continue
             val = initial_state[i]
