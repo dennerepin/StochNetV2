f4023f304721426cb5c54baa2505f2376ed0ad46
_dn <dennerepin@users.noreply.github.com>
2020-01-29 23:27:34 +0100

    added parameter randomisation and saving it to traces, + SIR model

---
+++ stochnet_v2/CRN_models/SIR.py
@@ -0,0 +1,163 @@
+import gillespy
+import numpy as np
+import math
+
+from stochnet_v2.CRN_models.base import BaseCRNModel
+
+
+class SIR(BaseCRNModel):
+    """Class for SIR network."""
+    params = {
+        'beta': '3.',
+        'gamma': '1.',
+    }
+
+    def __init__(self, endtime, timestep):
+        """
+        Initialize the model.
+
+        Parameters
+        ----------
+        endtime : endtime of simulations
+        timestep : time-step of simulations
+        """
+        super().__init__(
+            endtime=endtime,
+            timestep=timestep,
+            model_name="SIR",
+        )
+
+        beta = gillespy.Parameter(name='beta', expression=self.params['beta'])
+        gamma = gillespy.Parameter(name='gamma', expression=self.params['gamma'])
+        self.add_parameter([beta, gamma])
+
+        # Species
+        S = gillespy.Species(name='S', initial_value=100)
+        I = gillespy.Species(name='I', initial_value=100)
+        R = gillespy.Species(name='R', initial_value=100)
+        self.add_species([S, I, R])
+
+        # Reactions
+        infection = gillespy.Reaction(
+            name='infection',
+            reactants={S: 1, I: 1},
+            products={I: 2},
+            propensity_function='beta*S*I/(S+I+R)',
+        )
+        recover = gillespy.Reaction(
+            name='recover',
+            reactants={I: 1},
+            products={R: 1},
+            rate=gamma,
+        )
+        self.add_reaction([infection, recover])
+        nb_of_steps = int(math.ceil((endtime / timestep))) + 1
+        self.timespan(np.linspace(0, endtime, nb_of_steps))
+
+    def set_species_initial_value(self, species_initial_value):
+        """
+        Set initial values to species.
+
+        Parameters
+        ----------
+        species_initial_value : list or 1d array of values, size should be equal
+            to the number of species, the order should be coherent with theo order
+            of species returned by get_initial_state method.
+
+        Returns
+        -------
+        None
+
+        """
+        self.listOfSpecies['S'].initial_value = species_initial_value[0]
+        self.listOfSpecies['I'].initial_value = species_initial_value[1]
+        self.listOfSpecies['R'].initial_value = species_initial_value[2]
+
+    @staticmethod
+    def get_species_names():
+        """
+        Returns list of species names.
+
+        Returns
+        -------
+        list of all species names. The order of names should be coherent
+        with the list returned by get_initial_state method.
+
+        """
+        return ['S', 'I', 'R']
+
+    @staticmethod
+    def get_initial_state():
+        pass
+
+    @classmethod
+    def get_n_species(cls):
+        """Total number of species."""
+        return len(cls.get_species_names())
+
+    @classmethod
+    def get_initial_settings(cls, n_settings, sigm=None):
+        """
+        Generate a set of (random) initial states.
+
+        Parameters
+        ----------
+        n_settings : number of settings to produce.
+        sigm : not used
+
+        Returns
+        -------
+        array of n_settings initial states
+
+        """
+        n_species = cls.get_n_species()
+        settings = np.random.randint(low=30, high=200, size=(n_settings, n_species))
+        return settings
+
+    @classmethod
+    def get_histogram_bounds(cls, species_names_list=None):
+        """
+        Returns bounds for species histograms.
+
+        Parameters
+        ----------
+        species_names_list: not used
+
+        Returns
+        -------
+        histogram_bounds: list of [min, max] values for species
+
+        """
+        n_species_for_histogram = len(cls.get_species_for_histogram())
+        histogram_bounds = [[0.5, 200.5]] * n_species_for_histogram
+        return histogram_bounds
+
+    @staticmethod
+    def get_species_for_histogram():
+        """
+        Returns subset of species of interest.
+
+        Returns
+        -------
+        list of species names.
+
+        """
+        return ['S', 'I']
+
+    @classmethod
+    def get_randomized_parameters(cls, param_names, n_settings, sigm=0.5):
+        randomized = {}
+        for name in param_names:
+            if name not in cls.params:
+                raise KeyError(f"Could not find param {name} in {cls.__name__} class `params` dict.")
+            val = float(cls.params[name])
+
+            randomized[name] = np.random.uniform(val * (1. - sigm), val * (1. + sigm), n_settings)
+        return randomized
+
+    def set_parameters(self, params_dict):
+        for name, val in params_dict.items():
+            if name not in self.listOfParameters:
+                raise KeyError(
+                    f"Could not find {name} parameter in {self.__class__.__name__} model listOfParameters.")
+            self.set_parameter(name, str(val))
